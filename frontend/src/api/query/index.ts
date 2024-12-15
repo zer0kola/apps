bab7de7fb1e31296e090b76ca8b65a9a0e81ed56
zer0kola <zer0kola321@gmail.com>
2024-12-15 20:29:06 +0900

    feat: Update dependencies and enhance query options

--- frontend/src/api/query/index.ts
+++ frontend/src/api/query/index.ts
@@ -1,36 +1,47 @@
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import { nanoid } from 'nanoid'
+import apiInstance from '@/lib/api-instance'
+import { queryOptions, keepPreviousData } from '@tanstack/react-query'
+import type { Customer, Purchase, PurchaseWithId, PurchaseFrequencyRange, PurchaseFrequency } from '@/api/types'
 
 /**
- * 고객 목록 조회
- * @returns 고객 목록
- */
-export const useGetCustomerList = () => {
-  return useQuery({
-    queryKey: ['customers'],
-    queryFn: () => axios.get('/api/customers').then((res) => res.data),
-  })
-}
-
-/**
- * 구매 빈도 조회
+ * 구매 빈도 조회 옵션
+ * @param from 시작 날짜
+ * @param to 종료 날짜
+ * @param isInvalid 유효한 날짜인지
  * @returns 구매 빈도
  */
-export const useGetPurchaseFrequency = () => {
-  return useQuery({
-    queryKey: ['purchase-frequency'],
-    queryFn: () => axios.get('/api/purchase-frequency').then((res) => res.data),
+export const purchaseFrequencyQueryOptions = ({ from, to, isInvalid }: PurchaseFrequencyRange) =>
+  queryOptions({
+    queryKey: ['purchaseFrequency', from, to],
+    queryFn: () => apiInstance.get('purchase-frequency', { searchParams: { from, to } }).json<PurchaseFrequency[]>(),
+    throwOnError: true,
+    enabled: !isInvalid,
   })
-}
 
 /**
- * 고객 구매 내역 조회
- * @param customerId 고객 ID
- * @returns 고객 구매 내역
+ * 고객 목록 조회 옵션
+ * @param searchParams 검색 조건
+ * @returns 고객 목록
  */
-export const useGetCustomerPurchases = (customerId: string) => {
-  return useQuery({
-    queryKey: ['customer-purchases', customerId],
-    queryFn: () => axios.get(`/api/customer/${customerId}/purchases`).then((res) => res.data),
+export const customersQueryOptions = (searchParams = { sortBy: '', name: '' }) =>
+  queryOptions<Customer[], unknown, Customer[]>({
+    queryKey: ['customers', searchParams.sortBy, searchParams.name],
+    queryFn: () =>
+      apiInstance
+        .get('customers', {
+          searchParams,
+        })
+        .json<Customer[]>(),
+    select: (data) => data,
+    placeholderData: keepPreviousData,
   })
-}
+
+export const customerPurchasesQueryOptions = ({ id }: Partial<Pick<Customer, 'id'>>) =>
+  queryOptions<Purchase[], unknown, PurchaseWithId[]>({
+    queryKey: ['customerPurchases', id],
+    queryFn: () => apiInstance.get(`customers/${id}/purchases`).json<Purchase[]>(),
+    select: (data) => data.map((purchase) => ({ ...purchase, id: nanoid() })),
+    throwOnError: true,
+    enabled: !!id,
+    staleTime: 1000 * 60 * 5,
+  })
