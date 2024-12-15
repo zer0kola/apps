c5d5a386b6cf7c641c7cc74ef25a4eb628d4ff26
zer0kola <zer0kola321@gmail.com>
2024-12-15 21:57:22 +0900

    feat: 구매 횟수 차트 레이아웃 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import { useQuery } from '@tanstack/react-query'
-import {
-  // 고객 목록 조회
-  customersQueryOptions,
-  // 고객 구매 목록 조회
-  customerPurchasesQueryOptions,
-  // 구매 빈도 조회
-  purchaseFrequencyQueryOptions,
-} from '@/api/query'
+import { ChartSection } from '@/components/chart-section'
 
 function App() {
-  const { data, isLoading } = useQuery(customersQueryOptions())
-
-  return <main className="flex min-h-screen flex-col items-center justify-center bg-background"></main>
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-background">
+      <ChartSection />
+    </main>
+  )
 }
 
 export default App
---
+++ frontend/src/components/chart-section.tsx
@@ -0,0 +1,40 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { DatePickerWithRange } from '@/components/range-picker'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts'
+import { useQuery } from '@tanstack/react-query'
+import { purchaseFrequencyQueryOptions } from '@/api/query'
+
+export const ChartSection = () => {
+  const { data: purchaseFrequency } = useQuery(
+    purchaseFrequencyQueryOptions({ from: '2024-07-01', to: '2024-07-31', isInvalid: false }),
+  )
+
+  return (
+    <Card className="w-full h-96 max-w-6xl mx-auto mt-10 bg-white">
+      <CardHeader className="gap-2 flex-row items-center justify-between">
+        <CardTitle>가격대별 구매 횟수</CardTitle>
+        <DatePickerWithRange />
+      </CardHeader>
+      <CardContent>
+        <ChartContainer
+          config={{
+            count: {
+              label: '구매 횟수',
+              color: 'hsl(var(--chart-1))',
+            },
+          }}
+          className="h-[300px] w-full"
+        >
+          <BarChart data={purchaseFrequency} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="priceRange" />
+            <YAxis />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Bar dataKey="count" fill="var(--color-count)" />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
