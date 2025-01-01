3f7ab872ca83227fa7316c8d68bf61412c34c03b
zer0kola <zer0kola321@gmail.com>
2025-01-01 16:43:32 +0900

    feat: next client 모듈 생성

---
+++ client/apps/main/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { FC } from 'react';
+
+const Page: FC = () => {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4">
+      <h1>Playground</h1>
+    </main>
+  );
+};
+
+export default Page;
---
+++ client/apps/main/utils/index.ts
@@ -0,0 +1,8 @@
+// providers
+export * from './provider/authProvider';
+export * from './provider/theme-provider';
+export * from './provider/queryProvider';
+
+// supabase
+export * from './supabase/client';
+export * from './supabase/server';
