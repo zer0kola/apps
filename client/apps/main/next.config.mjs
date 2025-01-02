45f8d26e32e0d644d9e03b154eebc5623b9b5f3d
zer0kola <zer0kola321@gmail.com>
2025-01-03 00:58:34 +0900

    chore: init client next project

--- client/apps/main/next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@repo/ui", "@repo/api"],
-};
-
-export default nextConfig;
--- client/apps/main/utils/index.ts
+++
@@ -1,8 +0,0 @@
-// providers
-export * from './provider/authProvider';
-export * from './provider/theme-provider';
-export * from './provider/queryProvider';
-
-// supabase
-export * from './supabase/client';
-export * from './supabase/server';
